942ddc7e6f1edca6a1dfb478a21bf1688428118f
Vinicius Martins Ribeiro <vinimribeiro2004@gmail.com>
2024-04-05 13:42:59 -0300

    feat: create functions handle

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import { App } from './app'
+import { Toaster } from 'sonner'
+import './index.css'
+
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <Toaster richColors />
   </React.StrictMode>,
 )
